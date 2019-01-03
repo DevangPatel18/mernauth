15066cb6ac0b6bb00b8c0c33034fd033212c9dd3
dev637 <devang713@gmail.com>
2019-01-03 15:42:58 -0500

    Add redux, material icons, and other libraries

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <span className="material-icons"></span>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
