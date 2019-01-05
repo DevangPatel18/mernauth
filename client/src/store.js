2a5abb66b67f4808cde16d60e6269a3a79cd792f
dev637 <devang713@gmail.com>
2019-01-05 16:58:58 -0500

    Add auth logic and pull reducers into store
    Set authorization token and add relevant actions. Also pulled reducers into store.

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-const initialState = { testing: 'foo' }
+import rootReducer from './reducers'
+const initialState = {}
 const middleware = [thunk]
 
-const reducer = (state, action) => {
-  return {state}
-}
-
 const store = createStore(
-  (reducer),
+  rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
+    applyMiddleware(...middleware)
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__ ()
   )
 )
