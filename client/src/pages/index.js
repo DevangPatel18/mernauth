6b3d9a67cf583d37578c736408efb385fc45821a
dev637 <devang713@gmail.com>
2019-01-08 18:21:06 -0500

    Refactor routes using Router and onCreatePage api
    Use Router to implement routes. Also moved page components to components folder and used onCreatePage API to generate pages.

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,18 +1,26 @@
 import React from 'react'
 // import { Link } from 'gatsby'
 
-import "materialize-css/dist/css/materialize.min.css"
-import "materialize-css/dist/js/materialize.min.js"
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
 import Layout from '../components/layout'
 // import Image from '../components/image'
 import SEO from '../components/seo'
 import Landing from '../components/Landing'
+import { Router } from '@reach/router'
+import Register from '../components/Register'
+import Login from '../components/login'
+import Dashboard from '../components/dashboard'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <Landing />
-
+    <Router>
+      <Landing path="/" />
+      <Register path="/register" />
+      <Login path="/login" />
+      <Dashboard path="/dashboard" />
+    </Router>
     <span className="material-icons" />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }} />
   </Layout>
